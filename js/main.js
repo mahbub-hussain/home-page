e080d1acb92289f3596e02753f53f1ac93c2de81
mahfuz hussain <45997926+mahfuz-hussain@users.noreply.github.com>
2019-01-10 20:47:34 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+      $("#owl-demo").owlCarousel({
+
+      navigation : true,
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem : true,
+	  autoPlay : true,
+	  goToFirst : true,
+	  goToFirstSpeed : 1000,
+	  navigationText : ["<i class='fa fa-angle-left' aria-hidden='true'></i>","<i class='fa fa-angle-right' aria-hidden='true'></i>"],
+
+
+      });
+    });
+	
+	
+ new WOW().init();	
